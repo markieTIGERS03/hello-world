b4addbd24e712746ba1c9f28313dd8b06bb7208d
markieTIGERS03 <36550015+markieTIGERS03@users.noreply.github.com>
2019-07-09 21:30:22 +0800

    Create thread2.py

---
+++ thread2.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import pexpect
+from threading import *
+from pprint import pprint
+import re
+import sys
+import getopt
+
+index = 0
+
+class threadClass(Thread):
+    def run(self, index):
+        global ipadd
+        host = ipadd[index]
+
+        session = pexpect.spawn('ssh -o ConnectTimeout=10 -oStricthostKeyChecking=no -l ' + YOURUSERNAME + " " + host, timeout=15)
+        result = session.expect(['assword:', pexpect.TIMEOUT, pexpect.EOF])
+    
+        # Check for failure
+        if result != 0:
+            print 'ERROR: Timeout or unexpected reply from device: ' + host
+            print '\r'
+            print '\r'
+            print '\r'
+	    return
+
+        # Successfully got username prompt, enter username
+        session.sendline(YOURPASSWORD)
+        result = session.expect(Prompt1)
+        print '---Login to ' + host + ' successful'
+
+        #create logfile path
+        logpath = '/home/ivan/lab/logs/' + host + '.txt'
+        session.logfile = open(logpath, 'w')
+
+        session.sendline('enable')
+        result = session.expect('assword:')
+
+        session.sendline(YOURENABLE)
+        result = session.expect(Prompt2)
+        print '--- enable successful'
+
+        with open('commands', 'r') as g:
+            cmdlist = [line2.strip() for line2 in g]
+
+        for cmd in cmdlist:
+            session.sendline(cmd)
+            result = session.expect(Prompt2)
+            print '---Running command: ' + cmd
+
+        session.sendline()
+        result = session.expect(Prompt2)
+        session.sendline('exit')
+        print '---Successfully exited device ' + host
+        session.logfile.close()
+        print '---Log created: ' + logpath
+        print '\r'
+        print '\r'
+        print '\r'
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'u:p:e:h', ['username=', 'password=', 'enablepass=', 'help'])
+except getopt.GetoptError:
+    print 'Usage: xxxx.py -u <username> -p <password> -e <enablepass>'
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        print 'Usage: xxxx.py -u <username> -p <password> -e <enablepass>'
+        sys.exit(2)
+    elif opt in ('-u', '--username'):
+        YOURUSERNAME = arg
+    elif opt in ('-p', '--password'):
+        YOURPASSWORD = arg
+    elif opt in ('-e', '--enablepass'):
+        YOURENABLE = arg
+    else:
+        print 'Usage: xxxx.py -u <username> -p <password> -e <enablepass>'
+        sys.exit(2)
+
+try:
+    if YOURUSERNAME == "":
+        print YOURUSERNAME
+except:
+    print 'no username defined. use -u option to enter username'
+    exit()
+try:
+    if YOURPASSWORD == "":
+        print YOURUSERNAME
+except:
+    print 'no password defined. use -p option to enter password'
+    exit()
+try:
+    if YOURENABLE == "":
+        print YOURUSERNAME
+except:
+    print 'no enablepass defined. use -e option to enter enablepass'
+    exit()
+
+# Create regular expressions to match interfaces and OSPF
+Prompt1 = re.compile('>$')
+Prompt2 = re.compile('#$')
+
+
+with open('iplist', 'r') as f:
+    ipadd = [line.strip() for line in f]
+
+t = threadClass()
+t.run(0)
+t.run(1)
+t.run(2)
+
+print '--- end of test code'
+exit()
